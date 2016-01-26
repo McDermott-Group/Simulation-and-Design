df4380e0463f114bdd677eaf8753b2618c633802
Ivan Pechenezhskiy <pechenezhski@wisc.edu>
2016-01-26 13:33:09 -0600

    Add JPM simulation scripts
    Add MATLAB scripts to simulate high-power cavity drive.

--- GDS2FastHenry/run.py
+++ GDS2FastHenry/run.py
@@ -1,8 +1,8 @@
 import ImportGDSII as gds
 
-filebase = "Z:\\mcdermott-group\\MaskDesign\\50 Ohm JPM\\InductorSimulations\\"
+filebase = "C:\\Users\\5107-1\\Desktop\\Inductor Test\\"
 
 g = gds.GDSII()
 
-f1 = "Inductor_300pH_v2.gds"
-g.GDS2FH(filebase+f1, 0.1, '300 pH matching network inductor')
+f1 = "rfSquidGradiometer.gds"
+g.GDS2FH(filebase+f1, 0.1, 'Big gradiometer and flux bias coil for rf Squid readout')
