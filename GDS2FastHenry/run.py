298ba2a5c938e9feca0d03db2df23ab6656bb561
gribeill <gribeill@bbn.com>
2016-06-27 09:46:23 -0400

    Add example.

--- GDS2FastHenry/run.py
+++
@@ -1,8 +0,0 @@
-import ImportGDSII as gds
-
-filebase = "C:\\Users\\5107-1\\Desktop\\Inductor Test\\"
-
-g = gds.GDSII()
-
-f1 = "rfSquidGradiometer.gds"
-g.GDS2FH(filebase+f1, 0.1, 'Big gradiometer and flux bias coil for rf Squid readout')
